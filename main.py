b1fac4f9a9db88b359ad53e05a7655362e00f43e
Franklin <franklid@ds.ad.adp.com>
2017-07-05 10:32:44 -0700

    committing last changes

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] =  True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(120))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        self.submitted = False
+        #the error is coming from the fact that we've initialized
+        #submitted to false.
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        blog_name = request.form['blog']
+        new_blog = Blog(blog_name)
+        db.session.add(new_blog)
+        db.session.commit()
+
+    blogs = Blog.query.filter_by(submitted=False).all()
+    submitted_blogs = Blog.query.filter_by(submitted=True).all()
+    return render_template('todos.html',title="Build a Blog", 
+        blogs=blogs, submitted_blogs=submitted_blogs)
+
+
+@app.route('/newpost', methods=['POST'])
+def post_blog():
+
+    task_id = int(request.form['blog-id'])
+    blog = Blog.query.get(blog_id)
+    blog.submitted = True
+    db.session.add(blog)
+    db.session.commit()
+
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run()
