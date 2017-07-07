5ee3e29aca9148aacdb90b9bd5582776edbd270b
Franklin <franklid@ds.ad.adp.com>
2017-07-07 11:28:38 -0700

    updated code to have titles, bodies and place titles in headers

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ app.config['DEBUG'] =  True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-#app.secret_key = 'y337kGcys&zP3B'
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.Boolean)
+    body = db.Column(db.String(120))
+    submitted = db.Column(db.Boolean)
     
 
-    def __init__(self, title):
+    def __init__(self, title, body):
         self.title = title
-        self.body = False
+        self.body = body
+        self.submitted = False
 
 
 
@@ -25,21 +26,24 @@ class Blog(db.Model):
 def index():
 
     if request.method == 'POST':
-        blog_name = request.form['blog']
-        new_blog = Blog(blog_name)
+        blog_name_title = request.form['blog_title']
+        blog_name_body = request.form['blog_body']        
+        new_blog = Blog(blog_name_title, blog_name_body)        
         db.session.add(new_blog)
         db.session.commit()
 
-    blogs = Blog.query.filter_by(body=False).all()
-    submitted_blogs = Blog.query.filter_by(body=True).all()
+    blogs = Blog.query.filter_by(submitted=False).all()
+    submitted_blogs = Blog.query.filter_by(submitted=True).all()
     return render_template('todos.html',title="Build a Blog", blogs=blogs, submitted_blogs=submitted_blogs)
 
 
 @app.route('/newpost', methods=['POST'])
 def post_blog():
 
-    blog_id = int(request.form['blog-id'])
+    blog_id = int(request.form['blog-id-title'])
     blog = Blog.query.get(blog_id)
+    blog_body = int(request.form['blog-id-body'])
+    blog = Blog.query.get(blog_body)    
     db.session.add(blog)
     db.session.commit()
 
